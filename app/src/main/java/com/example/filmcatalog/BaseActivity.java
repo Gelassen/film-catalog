7f0931a127e1e8073f59334c84827f65d4f80c75
Dmitry Kazakov <dmitriikazakov@gmail.com>
2018-03-10 11:01:20 +0300

    Add styles

--- app/src/main/java/com/example/filmcatalog/BaseActivity.java
+++ app/src/main/java/com/example/filmcatalog/BaseActivity.java
@@ -3,6 +3,7 @@ package com.example.filmcatalog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 
 public abstract class BaseActivity<T extends BasePresenter> extends AppCompatActivity {
 
@@ -11,6 +12,9 @@ public abstract class BaseActivity<T extends BasePresenter> extends AppCompatAct
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        getSupportActionBar().hide();
 
         presenter = (T) getLastCustomNonConfigurationInstance();
         if (presenter == null) {
